633b902d730c16b8c8303d36788df7567895518b
Jean-Marco Alameddine <jean-marco.alameddine@tu-dortmund.de>
2022-10-24 16:58:12 +0200

    initial commit

---
+++ create_plots.py
@@ -0,0 +1,160 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import glob
+from corsikaio import CorsikaFile
+import sys
+
+from yaml import safe_load
+import datetime
+import time
+
+from scripts import *
+
+if (len(sys.argv) != 4):
+    print("Usage: create_plots.py PATH_TO_C7_DAT PATH_TO_C8_FOLDER OUTPUT_NAME")
+    assert(False)
+
+C7_PATHS = [sys.argv[1]]
+C8_PATHS = [sys.argv[2]]
+OUTPUT_NAME = sys.argv[3]
+
+labels = ["CORSIKA 7", "CORSIKA 8"]
+colors = ['orange', 'blue']
+
+CREATE_CHARGE_EXCESS_PLOTS = True
+CREATE_XMAX_PLOTS = True
+CREATE_LONGITUDINAL_PLOTS = True
+
+if not os.path.exists(OUTPUT_NAME):
+    os.makedirs(OUTPUT_NAME)
+
+### Read in C8
+
+def X_C8(profiles, NUM_SHOWERS):
+    'returns array of length n describing the X bins'
+    return profiles['X'][:int(len(profiles['X']) / NUM_SHOWERS)]
+
+def profile_C8(profiles, label, NUM_SHOWERS):
+    'returns array of shape m * n, where m is the number of profiles. every row has n entries which describe the longitudinal profile '
+    return profiles[label].to_numpy().reshape(NUM_SHOWERS, -1)
+
+
+C8_DATA = []
+C8_runtimes = []
+C8_SHOWER_NUMBERS = []
+
+for PATH in C8_PATHS:
+    print(f"Reading in C8 profiles from folder {PATH}")
+    profiles= []
+    for path in glob.glob(f"{PATH}/*/*/profile/profile.parquet"):
+        profiles.append(pd.read_parquet(path))
+    C8_DATA.append(pd.concat(profiles))
+    num_showers = len(profiles)
+    C8_SHOWER_NUMBERS.append(num_showers)
+    runtimes = []
+    for path in glob.glob(f"{PATH}/*/*/summary.yaml"):
+        with open(path, 'r') as f:
+            time_object = safe_load(f)['runtime']
+            if (type(time_object) is float):
+                runtimes.append(time_object)
+            else:
+                str_obj = time.strptime(time_object,'%H:%M:%S')
+                str_obj_in_sec = datetime.timedelta(hours=str_obj.tm_hour,minutes=str_obj.tm_min,seconds=str_obj.tm_sec).total_seconds()
+                runtimes.append(str_obj_in_sec)
+    sum_runtimes = sum(runtimes)
+    C8_runtimes.append(sum_runtimes)
+    print(f"Sum of all runtimes for C8 simulation: {sum_runtimes}, which corresponds to a time per shower of {sum_runtimes/num_showers}")
+
+
+### Read in C7
+
+def X_C7(df, NUM_SHOWERS):
+    'returns array of length n describing the X bins'
+    return df['X'][:int(len(df['X']) / NUM_SHOWERS)]
+
+def profile_C7(df, label, NUM_SHOWERS):
+    'returns array of shape m * n, where m is the number of profiles. every row has n entries which describe the longitudinal profile '
+    return df[label].to_numpy().reshape(NUM_SHOWERS, -1)
+
+C7_DATA = []
+C7_SHOWER_NUMBERS = []
+
+for PATH in C7_PATHS:
+    print(f"Reading in C7 profiles from file {PATH}")
+    C7_df = pd.DataFrame()
+    NUM_C7_SHOWERS = 0
+
+    with CorsikaFile(PATH) as f:
+        NUM_SHOWERS = int(f.run_header["n_showers"])
+        for e in f:
+            C7_df = C7_df.append(pd.DataFrame({"X":e.longitudinal['vertical_depth'],
+                 "electron":e.longitudinal['n_e_minus'],
+                 "positron":e.longitudinal['n_e_plus'],
+                 "photon":e.longitudinal['n_photons'],
+                 "charged":e.longitudinal['n_charged'],
+                 "hadron":e.longitudinal['n_hadrons'],
+                 "muplus":e.longitudinal['n_mu_plus'],
+                 "muminus":e.longitudinal['n_mu_minus']})
+                    )
+    C7_SHOWER_NUMBERS.append(NUM_SHOWERS)
+    C7_DATA.append(C7_df)
+
+        
+
+# generate grammage
+
+grammage = []
+for df, NUM in zip(C7_DATA, C7_SHOWER_NUMBERS):
+    grammage.append(X_C7(df, NUM))
+
+for df, NUM in zip(C8_DATA, C8_SHOWER_NUMBERS):
+    grammage.append(X_C8(df, NUM))
+
+### plot charge excess
+
+if (CREATE_CHARGE_EXCESS_PLOTS):
+    print("Create charge excess plots")
+    charge_excess = []
+
+    for df, NUM in zip(C7_DATA, C7_SHOWER_NUMBERS):
+        profile_electron = profile_C7(df, 'electron', NUM)
+        profile_positron = profile_C7(df, 'positron', NUM)
+        charge_excess.append((profile_electron - profile_positron) / (profile_electron + profile_positron))
+
+    for df, NUM in zip(C8_DATA, C8_SHOWER_NUMBERS):
+        profile_electron = profile_C8(df, 'electron', NUM)
+        profile_positron = profile_C8(df, 'positron', NUM)
+        charge_excess.append((profile_electron - profile_positron) / (profile_electron + profile_positron))
+
+    plot_long_hist_ratio(grammage, charge_excess, labels, colors, f"Charge excess")
+    plt.savefig(f"{OUTPUT_NAME}/charge_excess.png", dpi=300)
+
+### create X_max profiles
+
+if (CREATE_XMAX_PLOTS):
+    print("Create Xmax plots")
+
+    for p_type in ['electron', 'positron', 'photon', 'charged', 'muminus', 'muplus', 'hadron']:
+        profiles = []
+        for df, NUM in zip(C7_DATA, C7_SHOWER_NUMBERS):
+            profiles.append(profile_C7(df, p_type, NUM))
+        for df, NUM in zip(C8_DATA, C8_SHOWER_NUMBERS):
+            profiles.append(profile_C8(df, p_type, NUM))
+        plot_Xmax_hist_ratio(grammage, profiles, labels, colors, f"Xmax distribution for {p_type}", NUM_BINS=50)
+        plt.savefig(f"{OUTPUT_NAME}/Xmax_{p_type}.png", dpi=300)
+
+### plot longitudinal profiles
+
+if (CREATE_LONGITUDINAL_PLOTS):
+    print("Create longitudinal plots")
+    for p_type in ['electron', 'positron', 'photon', 'charged', 'muminus', 'muplus', 'hadron']:
+        profiles = []
+        for df, NUM in zip(C7_DATA, C7_SHOWER_NUMBERS):
+            profiles.append(profile_C7(df, p_type, NUM))
+        for df, NUM in zip(C8_DATA, C8_SHOWER_NUMBERS):
+            profiles.append(profile_C8(df, p_type, NUM))
+        plot_long_hist_ratio(grammage, profiles, labels, colors, f"Longituginal profile for {p_type}")
+        plt.savefig(f"{OUTPUT_NAME}/long_{p_type}.png", dpi=300)
+
---
+++ scripts.py
@@ -0,0 +1,131 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+from corsikaio import CorsikaFile
+
+def plot_long_hist(X, profile, title=''):    
+    '''
+    X: len n, which describes the X bins
+    profile: array of shape m * n, where m is the number of profiles. every row has n entries which describe the longitudinal profile
+    '''
+    plt.fill_between(X, np.percentile(profile.T, q=25, axis=1), np.percentile(profile.T, q=75, axis=1), step='pre', alpha=0.5, color='blue')
+    plt.step(X, np.percentile(profile.T, q=50, axis=1), color='blue', label='CORSIKA 8 (07ec65d5)')
+    
+    plt.grid()
+    plt.legend(fontsize=12)
+    plt.xlabel('grammage', fontsize=12)
+    plt.ylabel('number particles', fontsize=12)
+    plt.xlim(0, 1000)
+    plt.title(title, fontsize=14)
+    plt.tight_layout()
+
+def plot_long_hist_multiple(X_list, profile_list, label_list, color_list, title=''):    
+    '''
+    X: len n, which describes the X bins
+    profile: array of shape m * n, where m is the number of profiles. every row has n entries which describe the longitudinal profile
+    '''
+    for X, profile, label, color in zip(X_list, profile_list, label_list, color_list):
+        plt.fill_between(X, np.percentile(profile.T, q=25, axis=1), np.percentile(profile.T, q=75, axis=1), step='pre', alpha=0.5, color=color)
+        plt.step(X, np.percentile(profile.T, q=50, axis=1), label=label, color=color)
+    
+    plt.grid()
+    plt.legend(fontsize=12)
+    plt.xlabel('grammage', fontsize=12)
+    plt.ylabel('number particles', fontsize=12)
+    plt.xlim(0, 1000)
+    plt.title(title, fontsize=14)    
+    plt.tight_layout()
+
+def plot_long_hist_ratio(X_list, profile_list, label_list, color_list, title=''):    
+    '''
+    X: len n, which describes the X bins
+    profile: array of shape m * n, where m is the number of profiles. every row has n entries which describe the longitudinal profile
+    '''
+    assert(len(X_list) > 1)
+
+    fig, ax = plt.subplots(2, 1, gridspec_kw={'height_ratios': [2, 1]}, figsize=(15,10),sharex=True)
+    
+    ax[0].tick_params('x', labelbottom=False) # only for last plot
+    for X, profile, label, color in zip(X_list, profile_list, label_list, color_list):
+        ax[0].fill_between(X, np.percentile(profile.T, q=25, axis=1), np.percentile(profile.T, q=75, axis=1), step='pre', alpha=0.5, color=color)
+        ax[0].step(X, np.percentile(profile.T, q=50, axis=1), label=label, color=color)
+    ax[0].grid(which='both')
+    ax[0].set_ylabel('# particles', fontsize=14)
+    
+    ax[0].legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower left", ncol=2, fontsize=14)
+
+    for X, profile, label, color in zip(X_list[1:], profile_list[1:], label_list[1:], color_list[1:]):
+        # find common X values for base and compare
+        X_common, indices_base, indices_compare = np.intersect1d(X_list[0], X, return_indices=True)
+        vals_base = np.percentile(profile_list[0].T[indices_base], q=50, axis=1)
+        vals_compare = np.percentile(profile.T[indices_compare], q=50, axis=1)
+        ax[1].step(X_common, (vals_compare - vals_base)/vals_base, label=label, color=color)   
+    ax[1].grid(which='both')
+    fig.subplots_adjust(hspace=0.05)
+    ax[1].set_xlabel(r"grammage / g/cm²", fontsize=14)
+    ax[1].set_ylabel(f"ratio to {label_list[0]}", fontsize=14)
+    ax[1].set_ylim(-0.2, 0.2)
+    fig.suptitle(title, fontsize=14)
+    fig.tight_layout()
+        
+
+def plot_Xmax_hist(X_list, profile_list, label_list, color_list, title='', NUM_BINS=15):
+    for X, profile, label, color in zip(X_list, profile_list, label_list, color_list):
+        max_list = []
+        for prof in profile:
+            max_list.append(X[np.argmax(prof)])
+        plt.hist(max_list, bins=NUM_BINS, histtype='step', label=label, color=color)
+    plt.legend(fontsize=12)
+    plt.xlabel('X_max / grammage', fontsize=12)
+    plt.suptitle(title, fontsize=14)
+    plt.tight_layout()
+
+def plot_Xmax_hist_ratio(X_list, profile_list, label_list, color_list, title='', NUM_BINS=15):
+    assert(len(X_list) > 1)
+    fig, ax = plt.subplots(2, 1, gridspec_kw={'height_ratios': [2, 1]}, figsize=(15,10),sharex=True)
+    
+    ax[0].tick_params('x', labelbottom=False) # only for last plot
+    
+    X_max_lists = []
+   
+    X_max_mean_list = []
+
+    min_bin = +np.inf
+    max_bin = -np.inf
+   
+
+    for X, profile in zip(X_list, profile_list):
+        X_max = []
+        for prof in profile:
+            X_max.append(X[np.argmax(prof)])
+        min_bin = min(min_bin, min(X))
+        max_bin = max(max_bin, max(X))
+        X_max_lists.append(X_max)
+        X_max_mean_list.append(np.mean(X_max))
+        
+    bins = np.linspace(min_bin, max_bin, NUM_BINS)
+    
+    n_list = []
+    
+    for X_max_list, label, color, X_max_mean in zip(X_max_lists, label_list, color_list, X_max_mean_list):
+        n, _, _ = ax[0].hist(X_max_list, bins=bins, histtype='step', color=color, label=f"{label}, mean(X_max) = {X_max_mean:.2f} g/cm²")
+        n_list.append(n)
+        
+    ax[0].grid(which='both')
+    ax[0].set_ylabel('# showers', fontsize=14)
+    ax[0].legend(bbox_to_anchor=(0, 1, 1, 0), loc="lower left", ncol=2, fontsize=14)
+    
+    for n, label, color in zip(n_list[1:], label_list[1:], color_list[1:]):
+        ax[1].step(bins[1:], (n - n_list[0]) / n_list[0], color=color)
+    ax[1].grid(which='both')
+    fig.subplots_adjust(hspace=0.05)
+    ax[1].set_xlabel(r"grammage / g/cm²", fontsize=14)
+    ax[1].set_ylabel(f"ratio to {label_list[0]}", fontsize=14)
+    ax[1].set_ylim(-0.2, 0.2)
+
+    plt.legend(fontsize=12)
+    plt.xlabel('X_max / grammage', fontsize=12)
+    fig.suptitle(title, fontsize=14)
+    fig.tight_layout()
+
